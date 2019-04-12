c5c2556214d03cd2eb046fa3538e834fbdc61997
= <=>
2019-04-12 14:33:03 +0800

    Updated

--- src/views/GTI/GE.vue
+++ src/views/GTI/GE.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div style="background:#eee;padding: 20px;">
+    <div v-if="this.industryOptions.length>0 && this.countryOptions.length>0 && this.countryOptions2.length>0" style="background:#eee;padding: 20px;">
       <Form ref="formData" :model="formData" :rules="ruleInline" inline>
         <FormItem prop="e_country" label="APEC Economy">
           <Select
@@ -263,7 +263,7 @@ export default {
       tabValue: "Table",
       optionDisabled: false,
       optionDisabled2: false,
-      btnDisabled: false,
+      btnDisabled: true,
       pieDisabled: false,
       barDisabled: false,
       treeMapDisabled: false,
@@ -355,11 +355,13 @@ export default {
     };
   },
   mounted() {
+    this.getIndustry();
+    this.getCountry();
     this.yearOptions = yearOptions;
-    this.industryOptions = this.industry;
-    this.countryOptions = [...this.country];
-    this.countryOptions.shift();
-    this.countryOptions2 = this.country;
+    // this.industryOptions = this.industry;
+    // this.countryOptions = [...this.country];
+    // this.countryOptions.shift();
+    // this.countryOptions2 = this.country;
   },
   computed: {
     ...mapState(["pid", "fid", "page", "industry", "country"])
@@ -406,6 +408,7 @@ export default {
               let rp_data = response.data.data;
               that.tableData = rp_data.data;
               that.total = rp_data.total;
+              that.btnDisabled = false
               that.get3Ddata();
             })
             .catch(function(error) {
@@ -415,6 +418,58 @@ export default {
         } else {
           // this.$Message.error('Fail!');
         }
+      });
+    },
+    getIndustry() {
+      let that = this
+      ajax({
+          url: global.DEV_HOST + '/getIndustry',
+      })
+      .then(function (response) {
+          // let data = response.data.data
+          let newData = [
+            [ { 'en_type':'I1', 'industry':'Agriculture, hunting, forestry and fishing' } ], [ { 'en_type':'I2', 'industry':'Mining and quarrying' } ], [ { 'en_type':'I3', 'industry':'Food products, beverages and tobacco'}, { 'en_type':'I4', 'industry':'Iextiles and textile products,leather and footwear'}, { 'en_type':'I5', 'industry':'Wood and products of wood and cork'}, { 'en_type':'I6', 'industry':'Pulp, paper, paper products, printing and publishing'}, { 'en_type':'I7', 'industry':'Coke, refined petroleum products and nuclear fuel'}, { 'en_type':'I8', 'industry':'Chemicals'}, { 'en_type':'I9', 'industry':'Rubber and plastics products'}, { 'en_type':'I10', 'industry':'Other non-metallic mineral products'}, { 'en_type':'I11', 'industry':'Basic metals'}, { 'en_type':'I12', 'industry':'Fabricated metal products, except machinery and equipment'}, { 'en_type':'I13', 'industry':'Machinery and equipment, nec'}, { 'en_type':'I14', 'industry':'Computer, electronic and optical equipment'}, { 'en_type':'I15', 'industry':'Electrical machinery and apparatus, nec'}, { 'en_type':'I16', 'industry':'Motor vehicles, trailers and semi-trailers'}, { 'en_type':'I17', 'industry':'Other transport equipment'}, { 'en_type':'I18', 'industry':'Manufacturing nec  recycling (include Furniture)' } ], [ { 'en_type':'I19', 'industry':'Electricity, gas and water supply' }, { 'en_type':'I20', 'industry':'Construction' }, { 'en_type':'I21', 'industry':'Wholesale and retail tarde' }, { 'en_type':'I22', 'industry':'Hotels and restaurants' }, { 'en_type':'I23', 'industry':'Iransport and storage', }, { 'en_type':'I24', 'industry':'Post and Telecommunications' }, { 'en_type':'I25', 'industry':'Finance and insurance' }, { 'en_type':'I26', 'industry':'Real estate activities' }, { 'en_type':'I27', 'industry':'Renting of Machinery and equipment' }, { 'en_type':'I28', 'industry':'Computer and related activities' }, { 'en_type':'I29', 'industry':'R&D and other business activities' }, { 'en_type':'I30', 'industry':'Public administration and defence, compulsory social security' }, { 'en_type':'I31', 'industry':'Education' }, { 'en_type':'I32', 'industry':'Health and social work' }, { 'en_type':'I33', 'industry':'Other community, social and personal services' }, { 'en_type':'I34', 'industry':'Private households with employed persons & extra-territorial organizations & bodies' } ]
+          ]
+          // let echartIndustry = []
+          // newData = Object.assign([], data);
+          // data.forEach((item, index) => {
+          //   item.forEach(()=>{
+          //     console.log(item);
+          //       // newData[index].push({
+          //       //     value: item.en_type,
+          //       //     label: item.industry
+          //       // })
+          //     // echartIndustry.push(item.en_type)
+          //     })
+          // });
+          // console.log(data);
+          // that.setindustry(newData);
+          that.industryOptions = newData;
+      }).catch(function (error) {
+          console.log(error);
+      });
+    },
+    getCountry() {
+      let that = this
+      ajax({
+          url: global.DEV_HOST + '/getCountry',
+      })
+      .then(function (response) {
+          let data = response.data.data
+          let newData = []
+          // let echartIndustry = []
+          data.forEach((item, index) => {
+              newData.push({
+                  value: item.country,
+                  label: item.en_name
+              });
+          })
+          // that.setcountry(newData);
+          that.countryOptions = [...newData];
+          that.countryOptions.shift();
+          that.countryOptions2 = newData;
+      }).catch(function (error) {
+          console.log(error);
       });
     },
     handleReset(){
@@ -494,28 +549,28 @@ export default {
       }
     },
     exportData() {
-      let data = this.formData
-      ajax({
-          method: 'GET',
-          url: global.DEV_HOST + '/export',
-          params: data,
-          responseType: 'blob'
-      }).then(function (response) {
-          let url = window.URL.createObjectURL(response.data);
-          // window.location.href = url;
-          var link = document.createElement('a');
-          // link.download = that.text + ' ' + that.subtext;
-          link.href = url;
-          link.click();
-          window.URL.revokeObjectURL(link.href)
-      }).catch(function (error) {
-          console.log(error);
-      });
-    },
-    // this.$refs.tableData.exportCsv({
-    //     filename: 'Gross trade indicators(Unit: million $)'+'Gross exports ($): by APEC economy, trading partner, industry, year'
+    //   let data = this.formData
+    //   ajax({
+    //       method: 'GET',
+    //       url: global.DEV_HOST + '/export',
+    //       params: data,
+    //       responseType: 'blob'
+    //   }).then(function (response) {
+    //       let url = window.URL.createObjectURL(response.data);
+    //       // window.location.href = url;
+    //       var link = document.createElement('a');
+    //       // link.download = that.text + ' ' + that.subtext;
+    //       link.href = url;
+    //       link.click();
+    //       window.URL.revokeObjectURL(link.href)
+    //   }).catch(function (error) {
+    //       console.log(error);
     //   });
     // },
+    this.$refs.tableData.exportCsv({
+        filename: 'Gross trade indicators(Unit: million $)'+'Gross exports ($): by APEC economy, trading partner, industry, year'
+      });
+    },
     changeRadio(val) {
       if (val === "3D") {
         this.get3Ddata();
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -36,17 +36,21 @@ export default {
     return {
       component: null,
       path:'',
+      // loading:true,
       // component
       // components:'GE',
     }
   },
   computed: {
-    ...mapState(['pid', 'fid', 'page']),
+    ...mapState(['pid', 'fid', 'page','industry','country']),
     getComponent(){
-      if(this.page ===''){
-        return 'GTI_GE'
-      }
-      return this.page;
+      // console.log(this.industry);
+      // console.log(this.country);
+      // if(!this.industry && !this.country){
+        if(this.page ===''){
+          return 'GTI_GE'
+        }
+        return this.page;
     }
   },
 
@@ -73,7 +77,6 @@ export default {
       })
       .then(function (response) {
           // let data = response.data.data
-          
           let newData = [
             [ { 'en_type':'I1', 'industry':'Agriculture, hunting, forestry and fishing' } ], [ { 'en_type':'I2', 'industry':'Mining and quarrying' } ], [ { 'en_type':'I3', 'industry':'Food products, beverages and tobacco'}, { 'en_type':'I4', 'industry':'Iextiles and textile products,leather and footwear'}, { 'en_type':'I5', 'industry':'Wood and products of wood and cork'}, { 'en_type':'I6', 'industry':'Pulp, paper, paper products, printing and publishing'}, { 'en_type':'I7', 'industry':'Coke, refined petroleum products and nuclear fuel'}, { 'en_type':'I8', 'industry':'Chemicals'}, { 'en_type':'I9', 'industry':'Rubber and plastics products'}, { 'en_type':'I10', 'industry':'Other non-metallic mineral products'}, { 'en_type':'I11', 'industry':'Basic metals'}, { 'en_type':'I12', 'industry':'Fabricated metal products, except machinery and equipment'}, { 'en_type':'I13', 'industry':'Machinery and equipment, nec'}, { 'en_type':'I14', 'industry':'Computer, electronic and optical equipment'}, { 'en_type':'I15', 'industry':'Electrical machinery and apparatus, nec'}, { 'en_type':'I16', 'industry':'Motor vehicles, trailers and semi-trailers'}, { 'en_type':'I17', 'industry':'Other transport equipment'}, { 'en_type':'I18', 'industry':'Manufacturing nec  recycling (include Furniture)' } ], [ { 'en_type':'I19', 'industry':'Electricity, gas and water supply' }, { 'en_type':'I20', 'industry':'Construction' }, { 'en_type':'I21', 'industry':'Wholesale and retail tarde' }, { 'en_type':'I22', 'industry':'Hotels and restaurants' }, { 'en_type':'I23', 'industry':'Iransport and storage', }, { 'en_type':'I24', 'industry':'Post and Telecommunications' }, { 'en_type':'I25', 'industry':'Finance and insurance' }, { 'en_type':'I26', 'industry':'Real estate activities' }, { 'en_type':'I27', 'industry':'Renting of Machinery and equipment' }, { 'en_type':'I28', 'industry':'Computer and related activities' }, { 'en_type':'I29', 'industry':'R&D and other business activities' }, { 'en_type':'I30', 'industry':'Public administration and defence, compulsory social security' }, { 'en_type':'I31', 'industry':'Education' }, { 'en_type':'I32', 'industry':'Health and social work' }, { 'en_type':'I33', 'industry':'Other community, social and personal services' }, { 'en_type':'I34', 'industry':'Private households with employed persons & extra-territorial organizations & bodies' } ]
           ]
